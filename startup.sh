870f7f36abdd4f54bbd2e3cf273e6cb5d6627052
Akarunkumarthandapni09 <168422770+Akarunkumarthandapni09@users.noreply.github.com>
2025-08-16 15:02:52 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-echo "Preparing persistent data folder..."
-# Create persistent folder
-mkdir -p /home/data
 
-# Copy required files from wwwroot to persistent folder
-cp /home/site/wwwroot/app.py /home/data/
-cp /home/site/wwwroot/approval_model.pkl /home/data/
-cp /home/site/wwwroot/label_map.pkl /home/data/
-cp /home/site/wwwroot/prepare_data.py /home/data/
-cp /home/site/wwwroot/request_types_approvals.xlsx /home/data/
-cp /home/site/wwwroot/request_types_approvals_clean.csv /home/data/
-cp /home/site/wwwroot/train_model.py /home/data/
+export FLASK_APP=app.py
+export FLASK_ENV=production
 
-echo "Starting Flask app..."
-# Start Flask from the persistent folder
-cd /home/data
-gunicorn --bind 0.0.0.0 --timeout 600 app:app
+# Run Gunicorn on Azure port with 4 workers
+gunicorn --bind 0.0.0.0:$PORT app:app --workers 4 --timeout 120
